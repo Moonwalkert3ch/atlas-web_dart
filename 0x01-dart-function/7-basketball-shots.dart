07827dc937e4c08e5128e1fcc7a332beab36b2b5
Moonwalkert3ch <6620@holbertonstudents.com>
2024-10-07 18:50:23 -0500

    fixed operator to accept in the pointers

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,9 +1,13 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   // calculates teamA points
-  int teamAPoints = teamA['Free Throws']! * 1 + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
+  int teamAPoints = teamA['Free throws']! * 1 +
+                    teamA['2 pointers']! * 2 +
+                    teamA['3 pointers']! * 3;
 
   // calculates teamB points
-  int teamBPoints = teamB['Free Throws']! * 1 + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
+  int teamBPoints = teamB['Free throws']! * 1 +
+                    teamB['2 pointers']! * 2 +
+                    teamB['3 pointers']! * 3;
 
   if (teamAPoints > teamBPoints) {
     return 1;
