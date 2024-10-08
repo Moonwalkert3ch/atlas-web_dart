e2cd62d8fd389a91ff716b235d12d993c29c07f7
Moonwalkert3ch <6620@holbertonstudents.com>
2024-10-08 12:39:10 -0500

    made property private

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,36 @@
+class Password {
+  // private property
+  String _password;
+
+  // constructor initialize the property password
+  Password({required String password}) : _password = password;
+
+  // method to check if password is valid
+  bool isValid() {
+    if (_password == Null) return false;
+
+    String entry = _password;
+
+    // check length must be 8-16 chars
+    if (entry.length < 8 || entry.length > 16) {
+      return false;
+    }
+
+    // contains Uppercase letters
+    bool hasUpperCase = entry.contains(RegExp(r'[A-Z]'));
+
+    // contains lowercase letters
+    bool hasLowerCase = entry.contains(RegExp(r'[a-z]'));
+
+    // contains numbers
+    bool hasNumber = entry.contains(RegExp(r'[0-9]'));
+
+    return hasUpperCase && hasLowerCase && hasNumber;
+  }
+
+  // override the toString method
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
