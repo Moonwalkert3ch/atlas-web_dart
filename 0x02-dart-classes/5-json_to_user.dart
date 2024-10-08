005db4b50bee01d50c6b062bf3962567c81f4963
Moonwalkert3ch <6620@holbertonstudents.com>
2024-10-08 13:29:51 -0500

    setup class for static

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,35 @@
+class User {
+  // properties
+  String name;
+  int age, id;
+  double height;
+
+  // constructor
+  User({required this.id, required this.name, required this.age, required this.height});
+
+  // map representation of the user
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return new User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  // override toString()
+  @override
+  String toString() {
+    return 'User(id: $id, name: $name, age: $age, height: $height)';
+  }
+
+}
